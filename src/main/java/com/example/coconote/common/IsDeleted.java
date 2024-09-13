a0c7fdb4066d21d9e6b0d1cd0abe5ba92c345488
jsangmin99 <jsangmin99@gmail.com>
2024-09-13 09:14:03 +0900

    [FIX] 불필요한 어노테이션 제거

--- src/main/java/com/example/coconote/common/IsDeleted.java
+++ src/main/java/com/example/coconote/common/IsDeleted.java
@@ -1,13 +1,8 @@
 package com.example.coconote.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public enum IsDeleted {
     Y, N
 }
