c83d56b42eacf1aa1e9933c2790d371c42324968
sseho <kindseho@naver.com>
2024-09-13 17:48:12 +0900

    HC-23 [Feat] ThreadCreateReqDto 파일 생성

---
+++ src/main/java/com/example/coconote/api/thread/dto/requset/ThreadCreateReqDto.java
@@ -0,0 +1,13 @@
+package com.example.coconote.api.thread.dto.requset;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ThreadCreateReqDto {
+}
