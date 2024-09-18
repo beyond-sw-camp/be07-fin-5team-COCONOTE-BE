20c2c6777b08eeee0dbe3ea6aa521b5d3f216f83
JUNG-EUN1997 <rro0628@gmail.com>
2024-09-18 17:00:03 +0900

    HC-99 [FEAT] local redis 설정 진행 중

--- src/main/java/com/example/coconote/chatTest/dto/ChatRoom.java
+++ src/main/java/com/example/coconote/chatTest/dto/ChatRoom.java
@@ -1,21 +1,21 @@
 package com.example.coconote.chatTest.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
-
 
     public static ChatRoom create(String name) {
         ChatRoom chatRoom = new ChatRoom();
@@ -23,5 +23,4 @@ public class ChatRoom {
         chatRoom.name = name;
         return chatRoom;
     }
-
 }
---
+++ src/main/java/com/example/coconote/chatTest/redis/RedisPublisher.java
@@ -0,0 +1,17 @@
+package com.example.coconote.chatTest.redis;
+
+import com.example.coconote.chatTest.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
