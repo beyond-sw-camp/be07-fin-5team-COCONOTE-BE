3b786fc85dd51f39b1ec2362598ada1ac20c311d
JUNG-EUN1997 <rro0628@gmail.com>
2024-09-22 19:09:25 +0900

    [FIX] test용 config 생성

---
+++ src/main/java/com/example/coconote/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.coconote.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8082");
+        config.addAllowedOrigin("http://localhost:8088");
+        config.addAllowedOrigin("http://localhost:59543");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
