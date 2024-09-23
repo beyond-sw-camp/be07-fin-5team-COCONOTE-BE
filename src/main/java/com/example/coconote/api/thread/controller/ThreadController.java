34a9bc8b0c89e206c59e01cd6dd59e2b019bbfea
jsangmin99 <jsangmin99@gmail.com>
2024-09-23 12:10:52 +0900

    [HC-48 HC-118] 쓰레드 삭제 및 스케줄링을 통해 완전 삭제 추가

--- src/main/java/com/example/coconote/api/thread/controller/ThreadController.java
+++ src/main/java/com/example/coconote/api/thread/controller/ThreadController.java
@@ -14,23 +14,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/thread")
+@RequestMapping("/api/v1/thread")
 @RequiredArgsConstructor
 public class ThreadController {
     private final ThreadService threadService;
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public ResponseEntity<?> createThread(@RequestBody ThreadCreateReqDto dto) {
         ThreadResDto threadResDto = threadService.createThread(dto);
         CommonResDto commonResDto = new CommonResDto(HttpStatus.CREATED, "쓰레드가 성공적으로 생성되었습니다.", threadResDto);
         return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
     }
 
-    @GetMapping("list/{channelId}")
+    @GetMapping("/list/{channelId}")
     public ResponseEntity<?> listThreads(@PathVariable Long channelId, Pageable pageable) {
         Page<ThreadResDto> ThreadListResDtos = threadService.threadList(channelId, pageable);
         CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "쓰레드 목록이 성공적으로 반환되었습니다.", ThreadListResDtos);
         return new ResponseEntity<>(commonResDto, HttpStatus.OK);
     }
 
+    @DeleteMapping("/delete/{threadId}")
+    public ResponseEntity<?> deleteThread(@PathVariable Long threadId) {
+        threadService.deleteThread(threadId);
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "쓰레드가 성공적으로 삭제되었습니다.", null);
+        return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+    }
 }
