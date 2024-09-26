8c97283cee871fa9c123a268493b572c34743bfc
JUNG-EUN1997 <rro0628@gmail.com>
2024-09-26 14:47:41 +0900

    [FEAT] HC-143 Canvas 및 Block entity만 적용

--- src/main/java/com/example/coconote/api/canvas/block/dto/response/BlockListResDto.java
+++ src/main/java/com/example/coconote/api/canvas/block/dto/response/BlockListResDto.java
@@ -1,5 +1,6 @@
 package com.example.coconote.api.canvas.block.dto.response;
 
+import com.example.coconote.api.canvas.block.entity.Type;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,6 +17,13 @@ import java.util.List;
 public class BlockListResDto {
     private Long id;
     private String content;
+
+    private String feId; // front 종속 id
+    private Type type;
+
+    private String member;
+
     @Builder.Default
     private List<BlockListResDto> childBlock = new ArrayList<>();
 }
+
