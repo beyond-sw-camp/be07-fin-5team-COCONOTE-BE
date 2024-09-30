f8beb5267239d924b38ef34a51254cedf0fdac46
sseho <kindseho@naver.com>
2024-09-30 17:06:18 +0900

    [Feat] HC-147 stompHandler가 token검증 및 웹소켓 메시지 수신후에도 검증

--- src/main/java/com/example/coconote/config/WebSocketConfig.java
+++ src/main/java/com/example/coconote/config/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package com.example.coconote.config;
 
+import com.example.coconote.api.thread.thread.component.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -10,6 +13,9 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
@@ -20,6 +26,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/v1/ws-stomp").setAllowedOrigins("http://localhost:8080", "http://localhost:8082")
                 .withSockJS();
+    }
+
+//    stompHandler가 webSocket 앞단에서 token을 체크
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 
 
