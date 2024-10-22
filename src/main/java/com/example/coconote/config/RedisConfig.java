54d8a2e3a720b0299331361cfe339dc80d29aa83
jsangmin99 <jsangmin99@gmail.com>
2024-10-22 10:28:01 +0900

    [CI/CD] 개발 서버 Redis 추가

--- src/main/java/com/example/coconote/config/RedisConfig.java
+++ src/main/java/com/example/coconote/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.coconote.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.MessageListener;
@@ -14,10 +15,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
