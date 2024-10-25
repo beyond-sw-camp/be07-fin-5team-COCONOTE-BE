bc1ae657067635a8814c32d61eb9643bba745280
Jiho Kim <jiho3634@gmail.com>
2024-10-25 14:28:02 +0900

    [Fix] HC-200 SMANGER 강등 수정

--- src/main/java/com/example/coconote/api/workspace/workspaceMember/service/WorkspaceMemberService.java
+++ src/main/java/com/example/coconote/api/workspace/workspaceMember/service/WorkspaceMemberService.java
@@ -118,7 +118,7 @@ public class WorkspaceMemberService {
         if(workspaceMember.getIsDeleted().equals(IsDeleted.Y)) {
             throw new IllegalArgumentException("이미 워크스페이스에서 탈퇴한 회원입니다.");
         }
-        if (workspaceMember.getWsRole() == WsRole.PMANAGER || workspaceMember.getWsRole() == WsRole.SMANAGER) {
+        if (workspaceMember.getWsRole() == WsRole.PMANAGER) {
             throw new IllegalArgumentException("워크스페이스 소유자는 강등할 수 없습니다.");
         }
         Workspace workspace = workspaceMember.getWorkspace();
