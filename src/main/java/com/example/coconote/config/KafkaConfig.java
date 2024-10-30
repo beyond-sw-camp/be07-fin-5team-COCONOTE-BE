6e5931ba2e1d82705cb235620e04bda07516d461
jsangmin99 <jsangmin99@gmail.com>
2024-10-30 10:17:37 +0900

    [CICD] 카프카 설정

--- src/main/java/com/example/coconote/config/KafkaConfig.java
+++ src/main/java/com/example/coconote/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.example.coconote.config;
 
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -20,7 +21,6 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConfig {
-
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -28,14 +28,15 @@ public class KafkaConfig {
     private String groupId;
 
     @Value("${spring.kafka.consumer.auto-offset-reset}")
-    private String autoOffset;
+    private String autoOffSet;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffset);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffSet);
+
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
@@ -47,23 +48,33 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new FixedBackOff(3000L, 3));
+        // 새로운 DefaultErrorHandler 설정
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new FixedBackOff(3000L, 3)  // 3초 간격으로 3번 재시도
+        );
+
         factory.setCommonErrorHandler(errorHandler);
+
 
         return factory;
     }
 
+
+    //    ====Producer
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServer;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer); // 서버위치 넣어주는 것
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); //StringSerializer는, kafka에 있는걸 불러오기
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); //JsonSerializer도 kafka 안에 있는걸로
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
-    @Bean
+    @Bean // 템플릿 제작
     public KafkaTemplate<String, Object> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
     }
