5fdc167676d4681cdc2eec17133a6b4c4d2f829c
jsangmin99 <jsangmin99@gmail.com>
2024-10-30 14:46:04 +0900

    [CI/CD] kafka 설정

--- src/main/java/com/example/coconote/config/KafkaConfig.java
+++ src/main/java/com/example/coconote/config/KafkaConfig.java
@@ -60,14 +60,11 @@ public class KafkaConfig {
     }
 
 
-    //    ====Producer
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootStrapServer;
 
     @Bean
     public ProducerFactory<String, Object> producerFactory(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer); // 서버위치 넣어주는 것
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // 서버위치 넣어주는 것
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); //StringSerializer는, kafka에 있는걸 불러오기
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); //JsonSerializer도 kafka 안에 있는걸로
 
