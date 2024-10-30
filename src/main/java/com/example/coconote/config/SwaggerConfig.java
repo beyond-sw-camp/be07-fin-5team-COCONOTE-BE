6ef0ed9b83d84b1d7d54ed42f2f263c94d885b42
jsangmin99 <jsangmin99@gmail.com>
2024-10-31 00:08:33 +0900

    [CI/CD] 배포자동화  테스트

--- src/main/java/com/example/coconote/config/SwaggerConfig.java
+++ src/main/java/com/example/coconote/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Coconote 프로젝트 API")
                         .description("Coconote 백엔드 API 명세서")
-                        .version("v1"))
+                        .version("v2"))
                 .addServersItem(localServer)
                 .addServersItem(server)
                 .addSecurityItem(new SecurityRequirement().addList("JWT"))
