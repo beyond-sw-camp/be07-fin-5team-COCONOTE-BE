374d2b27df1b608e6084bd1878892e654e7f0af6
jsangmin99 <jsangmin99@gmail.com>
2024-11-01 14:48:42 +0900

    [FIX] 비공개  드라이브 리스트 문제해결

--- src/main/java/com/example/coconote/config/RedisConfig.java
+++ src/main/java/com/example/coconote/config/RedisConfig.java
@@ -18,10 +18,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${REDIS_HOST}")
+//    @Value("${REDIS_HOST}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${REDIS_PORT}")
+//    @Value("${REDIS_PORT}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     @Bean
