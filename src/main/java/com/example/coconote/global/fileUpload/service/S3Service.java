ad7c8078ae942f96619c9eb2b89cd9f33874585f
jsangmin99 <jsangmin99@gmail.com>
2024-11-05 16:15:56 +0900

    [FIX] 프로메테우스 환경 제거 및 파일 사이즈 5MB 로 변경

--- src/main/java/com/example/coconote/global/fileUpload/service/S3Service.java
+++ src/main/java/com/example/coconote/global/fileUpload/service/S3Service.java
@@ -77,7 +77,7 @@ public class S3Service {
     @Value("${aws.s3.bucket}")
     private String bucketName;
 
-    private static final long MAX_FILE_SIZE = 1 * 1024 * 1024; // 1MB
+    private static final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
 
     // 다중 파일에 대한 Presigned URL 생성
     public Map<String, String> generatePresignedUrls(List<FileUploadRequest> files, String email) {
